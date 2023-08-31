75a6db33cadba8d44fff16fe93ee03bf9cd25e51
Dabenniu13 <19064166@qq.com>
2023-08-30 21:27:35 -0400

    minor changes

--- src/main.py
+++ src/main.py
@@ -1,9 +1,8 @@
 import os
 from datetime import datetime
 import logging
-from fastapi import FastAPI, Form, Response
+from fastapi import FastAPI, Form
 from fastapi.middleware.cors import CORSMiddleware
-import numpy as np
 import cv2 as cv
 import json
 
@@ -47,6 +46,8 @@ async def videopathHandler(video_path: str= Form()):
         global cap
         if not os.path.exists(video_path):
             return {'error': 'Video file does not exist'}
+        if cap:
+            cap.release()
         cap = cv.VideoCapture(video_path)
         return {'frame_count': cap.get(cv.CAP_PROP_FRAME_COUNT), 'fps': cap.get(cv.CAP_PROP_FPS)}
     except Exception as e:
@@ -60,24 +61,22 @@ async def getFrame(num: int):
     # num in request already -1, so start from 0
     logger.debug(f'api/frame?num={num}')
     try:
-        frame_count = cap.get(cv.CAP_PROP_FRAME_COUNT)
-        if num < 0 or num > frame_count-1:
-            return {'error': 'Frame number out of bound'}
+        print(cap.get(cv.CAP_PROP_FRAME_COUNT))
+        #frame_count = cap.get(cv.CAP_PROP_FRAME_COUNT)
+        # if num < 0 or num > frame_count-1:
+        #     return {'error': 'Frame number out of bound'}
         cap.set(cv.CAP_PROP_POS_FRAMES, num)
         ret, frame = cap.read()
+        cv.imwrite(f'../frames/f_{num}.jpg',frame)
         if ret:
-            # cv.imwrite(f'./f_{num}.jpg', frame)
-            # frame = cv.imread(f'./f_{num}.jpg')
-            ret, frame_1d = cv.imencode('.jpg', frame)
-            # print(frame_1d)
-            frame_1d_int = map(lambda x: int(x), frame_1d)
-            # print(list(frame_1d_int))
+            ret, frame_1d = cv.imencode('.jpg', frame)  
+            frame_1d_int = map(lambda x: int(x), frame_1d)          
             # return Response(content=frame_1d, media_type='image/jpg')
             return {'res': json.dumps(list(frame_1d_int))}
         else:
-            return {'error': f'Can not read frame {num+1}'}
+            return {'error': f'Frame {num+1} reached video end'}
     except Exception as e:
         print('error')
         return error_handler(e)
-
+    
 
