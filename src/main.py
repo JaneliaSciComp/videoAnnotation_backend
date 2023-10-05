e36b0d1101e6ee5e16716cae5e128bb783cfc6a6
Xi Peng <xpengjanelia@gmail.com>
2023-10-04 20:28:11 -0400

    implemented connecting to postgreSQL server

--- src/main.py
+++ src/main.py
@@ -7,7 +7,8 @@ from fastapi.responses import FileResponse, Response
 from fastapi.middleware.cors import CORSMiddleware
 import cv2 as cv
 import json
-
+import psycopg
+from configparser import ConfigParser
 
 
 #set up logger
@@ -18,6 +19,40 @@ log_handler = logging.StreamHandler()
 log_handler.setFormatter(logging.Formatter('%(asctime)s | %(levelname)s: %(message)s'))
 logger.addHandler(log_handler)
 
+
+######### create connection to db ########
+def config(filename='../database.ini', section='postgresql'): #section matches the section name, the first line, in database.int
+    # create a parser
+    parser = ConfigParser()
+    # read config file
+    parser.read(filename)
+
+    # get section, default to postgresql
+    db_params = {}
+    if parser.has_section(section):
+        params = parser.items(section)
+        for param in params:
+            db_params[param[0]] = param[1]
+    else:
+        raise Exception('Section {0} not found in the {1} file'.format(section, filename))
+
+    return db_params
+
+
+try:
+    params = config()
+    logger.info('Connecting to the PostgreSQL database...')
+    conn = psycopg.connect(**params)
+    cur = conn.cursor()
+    cur.execute('SELECT current_schema()')
+    schema = cur.fetchone()
+    logger.info(f'Current schema: {schema}')
+except (Exception, psycopg.DatabaseError) as error:
+    logger.error(error)
+
+
+
+######## launch api server ########
 app = FastAPI()
 
 cap = None
@@ -67,9 +102,9 @@ async def getFrame(num: int):
         if num < 0:
             return {'error': 'Frame number out of bound'}
         cap.set(cv.CAP_PROP_POS_FRAMES, num)
-        logger.debug(f'cursor1: {cap.get(cv.CAP_PROP_POS_FRAMES)}')
+        # logger.debug(f'cursor1: {cap.get(cv.CAP_PROP_POS_FRAMES)}')
         ret, frame = cap.read()
-        logger.debug(f'cursor2: {cap.get(cv.CAP_PROP_POS_FRAMES)}')
+        # logger.debug(f'cursor2: {cap.get(cv.CAP_PROP_POS_FRAMES)}')
         if ret:
             ret, frame_1d = cv.imencode('.jpg', frame) 
             # return Response(content=frame_1d, media_type='image/jpg')
